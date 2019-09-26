4a0be9291fb65e4ac51e67d505f0655268b65780
Lina Kherchi <linakherchi@gmail.com>
2019-09-26 08:20:39 -0700

    editions

--- frontend/src/components/competition/competition_index.jsx
+++ frontend/src/components/competition/competition_index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './competition.css';
 import { withRouter } from 'react-router-dom';
+import competition1 from './../../competition1.jpg';
+import competition2 from './../../competition2.jpg';
+import competition4 from './../../competition4.jpg';
+
 
 
 class Competition extends React.Component{
@@ -13,9 +17,14 @@ class Competition extends React.Component{
 render(){
     return(
         <div id="competition-container">
-
-
-
+            <div className="background"><p id="trophee-competition">🏆</p>CodeRank Contest</div>
+            <button id="there-is-a-contest-coming-in">
+                <p id="join-competition">Join this 1 hour contest</p>
+                <div id="time-countdown">
+                    <p id="contest-number">Weekly Contest 1</p>
+                    <p id="starts-in"><i id="clock" class="material-icons">access_time</i>Starts in #countdown</p>
+                </div>
+            </button>
         </div>
     )
     }
